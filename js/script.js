2c79ea327df395f0243c3f360568e8d3abc27ff2
satya-sanjay2110 <satyasanjay1999@gmail.com>
2023-03-26 14:26:23 +0530

    added tv-details function

--- js/script.js
+++ js/script.js
@@ -75,6 +75,9 @@ async function displayMovieDetails() {
 
   const movie = await fetchAPIData(`movie/${movieId}`);
 
+  // overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
+
   const div = document.createElement('div');
   div.innerHTML = `
     <div class="details-top">
@@ -118,12 +121,94 @@ async function displayMovieDetails() {
       </ul>
       <h4>Production Companies</h4>
       <div class="list-group">${
-        movie.production_companies.map(company => `<li>${company.name}</li>`).join('')
+        movie.production_companies.map(company => `<span>${company.name}</span>`).join(', ')
       }</div>
     </div>
   `;
 
   document.querySelector('#movie-details').appendChild(div);
+}
+
+// display show details
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+
+  const show = await fetchAPIData(`tv/${showId}`);
+  
+  // overlay for background image
+  displayBackgroundImage('tv', show.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+    <div class="details-top">
+      <div>
+        ${
+          show.poster_path ? `<img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+            class="card-img-top"
+            alt="${show.name}"
+          />` : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${show.name}"
+          />`
+        }
+      </div>
+      <div>
+        <h2>${show.name}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${show.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">First Air Date: ${show.first_air_date} <br>Last Air Date: ${show.last_air_date}</p>
+        <p>${show.overview}</p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${
+            show.genres.map(genre => `<li>${genre.name}</li>`).join('')
+          }
+        </ul>
+        <a href="${show.homepage}" target="_blank" class="btn">Visit show Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Show Info</h2>
+      <ul>
+        <li><span class="text-secondary">Number of Episodes:</span> ${show.number_of_episodes}</li>
+        <li><span class="text-secondary">Number of Seasons:</span> ${show.number_of_seasons}</li>
+        <li><span class="text-secondary">Last Episode to Air:</span> ${show.last_episode_to_air.name}</li>
+        <li><span class="text-secondary">Status:</span> ${show.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">${
+        show.production_companies.map(company => `<span>${company.name}</span>`).join(', ')
+      }</div>
+    </div>
+  `;
+
+  document.querySelector('#show-details').appendChild(div);
+}
+
+// display Backdrop on details pages
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 }
 
 // fetch data from TMDB API
@@ -181,7 +266,7 @@ function init() {
       displayMovieDetails();
       break;
     case '/tv-details.html':
-      console.log('TV Details');
+      displayShowDetails();
       break;
     case '/search.html':
       console.log('Search');
