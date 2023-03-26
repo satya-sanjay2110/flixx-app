0df6d68fd5610354f5b0fc801ff75d423b8c4492
satya-sanjay2110 <satyasanjay1999@gmail.com>
2023-03-26 15:24:15 +0530

    slider function added on homepage

--- js/script.js
+++ js/script.js
@@ -134,7 +134,7 @@ async function displayShowDetails() {
   const showId = window.location.search.split('=')[1];
 
   const show = await fetchAPIData(`tv/${showId}`);
-  
+
   // overlay for background image
   displayBackgroundImage('tv', show.backdrop_path);
 
@@ -211,6 +211,53 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Display slider Movies
+async function displaySlider() {
+  const { results } = await fetchAPIData('movie/now_playing');
+  
+  results.forEach(movie => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(1)} / 10
+      </h4>
+    `;
+
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwiper();
+  })
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', { 
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      }
+    }
+  });
+}
+
 // fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   const API_KEY = '0186e45041252ec1255d254dbd474a4b';
@@ -257,6 +304,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
+      displaySlider();
       displayPopularMovies();
       break;
     case '/shows.html':
